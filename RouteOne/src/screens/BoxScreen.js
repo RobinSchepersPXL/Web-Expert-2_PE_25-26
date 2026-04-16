cc01416d251e106f6d79a508705287ba0cf6a9d0
RobinSchepersPXL <robin.schepers@student.pxl.be>
2026-04-16 20:44:06 +0200

    aan passen van de basis structuur en sreens toevoegen aan het project.

---
+++ RouteOne/src/screens/BoxScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function BoxScreen() {
+  return (
+    <View>
+      <Text>Box Screen</Text>
+    </View>
+  );
+}
---
+++ RouteOne/src/screens/CapsScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function RunListScreen() {
+  return (
+    <View>
+      <Text>Caps Screen</Text>
+    </View>
+  );
+}
